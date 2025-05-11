19f0c553811c23eeb2894d312e8b9687e0adb68a
Daniel Hug <daniel_hug@t-online.de>
2025-05-11 21:11:09 +0200

    minor ajustment of comments

--- ga/ga_usr_utilities.hpp
+++ ga/ga_usr_utilities.hpp
@@ -15,4 +15,6 @@ inline constexpr value_t rad2deg(value_t value) { return value * 180.0 / pi; }
 
 inline constexpr value_t sign(value_t value) { return (value >= 0.0) ? 1.0 : -1.0; }
 
+inline constexpr value_t kronecker(size_t i, size_t j) { return (i == j) ? 1.0 : 0.0; }
+
 } // namespace hd::ga
